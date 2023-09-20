713a2da93b95bdc31814c0f833d9c1de8f3c2221
Luigi Consentino <luigi.aconsentino@gmail.com>
2023-09-20 14:55:28 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description": "Marvel Studios anuncia novo filme do Homem de Ferro, confira!",
+    "photoCover":"https://gamerview.uai.com.br/wp-content/uploads/2020/06/marvels-iron-man-vr.jpg"
+  },
+  {
+    "id":"2",
+    "title": "Nova Série anunciada no Disney +",
+    "description": "Nova série da Marvel será lançada em Fevereiro de 2024!",
+    "photoCover":"https://www.thedigitalfix.com/wp-content/sites/thedigitalfix/2023/08/iron-man-movies-in-order.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Novo filme do Pantera Negra será lançado em breve",
+    "description": "Diretor da Marvel Studios afirma que novo filme do Pantera Negra está em produção!",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/F6482D58C8BB4D899FA4682BF27652C3E1BB73F5971E5F963994112F26A73690/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id":"4",
+    "title": "Nova filme do X-Men está em produção, afirma diretor",
+    "description": "O novo filme X-Men - The Force tem previsão de lançamento para Agosto de 2024.",
+    "photoCover":"https://images.immediate.co.uk/production/volatile/sites/3/2020/04/x-men-films-e30de66.jpg"
+  }
+]
